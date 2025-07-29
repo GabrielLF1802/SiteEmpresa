4ff666dde682e449ce9232976b9b9de50a3d5626
Gabriel <gabriellourencofaria@gmail.com>
2025-07-29 19:58:16 -0300

    configurações iniciais e instalação de arquivos

---
+++ app.js
@@ -0,0 +1,71 @@
+// Imports
+
+const express = require('express')
+const session= require('express-session')
+const bodyParser= require('body-parser')
+const {engine} = require('express-handlebars')
+const path = require('path')
+const flash = require('connect-flash')
+const mongoose = require('mongoose')
+const app= express()
+const db = require('./config/db')
+
+// Configs 
+    // Session
+    app.use(session({
+        secret:"appnode",
+        resave:true,
+        saveUninitialized:true
+    }))
+    
+    // Handlebars
+    app.engine('handlebars', engine({defaultLayout:'main',runtimeOptions: {
+    allowProtoPropertiesByDefault: true,
+    allowProtoMethodsByDefault: true
+  }}))
+    app.set('view engine', 'handlebars')
+
+    // Flash
+        app.use(flash());
+    
+    // Middleware
+    app.use((req,res,next)=>{
+        res.locals.success_msg= req.flash('success_msg')
+        res.locals.error_msg= req.flash('error_msg')
+        res.locals.error= req.flash('error')
+        res.locals.user= req.user || null
+        next()
+    })
+    // Body-parser
+        app.use(bodyParser.urlencoded({extended:false}))
+        app.use(bodyParser.json())
+    // Public
+        app.use(express.static(path.join(__dirname,'public')))
+    // Mongoose
+        mongoose.Promise= global.Promise
+        mongoose.connect(db.mongoURI).then(() => {
+            console.log('Conectado ao mongo')
+        }).catch((erro)=>{
+            console.log('Falha ao conectar' + erro)
+        })
+
+// Rotas
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+
+    
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 8081
+app.listen(PORT, ()=>{
+    console.log('Server Started!')
+} )
