a350ffd6b316f521c2c1d454ecd73dacb1ae2f52
Gabriel <gabriellourencofaria@gmail.com>
2025-07-31 10:46:01 -0300

    Verificação de dados no registro de usuário

--- routes/user.js
+++ routes/user.js
@@ -17,22 +17,52 @@ router.get('/register',(req,res)=>{
 })
 router.post('/register',(req,res)=>{
     // Possíveis Erros
+    erros= []
+    if(typeof req.body.senha==undefined|| !req.body.senha|| req.body.senha == null){
+        erros.push({texto:'Senha Inválida'})
+    }
+    if (!req.body.nome|| typeof req.body.nome == undefined || req.body.nome== null){
+        erros.push({texto:'Nome Inválido'})
+    }
+    if(!req.body.email||typeof req.body.email== undefined|| req.body.email== null){
+        erros.push({texto:'Email Inválido'})
+    }
+    if(!req.body.nasc|| typeof req.body.nasc== undefined|| req.body.nasc== null){
+        erros.push({texto:'Data de nascimento Inválida'})
+    }
+    if(req.body.senha != req.body.senha2){
+        erros.push({texto:'As senhas não conhecidem'})
+    }
+    if(req.body.senha.length< 4){
+        erros.push({texto:'Senha muito curta'})
+    }
+    if(erros.length>0){
+        res.render('user/register',{erros:erros})
+    }else{
+        Usuario.findOne({email:req.body.email}).then((usuario)=>{
+            if(usuario){
+                req.flash('error_msg','Email já cadastrado')
+                res.redirect('/user/register')
+            }else{
+                const newUser = new Usuario({
+                    nome: req.body.nome,
+                    email:req.body.email,
+                    nasc: req.body.nasc,
+                    senha: req.body.senha
+                })
+                newUser.save().then(()=>{
+                    req.flash('success_msg','Sucesso ao registrar o novo usuário')
+                    res.redirect('/user/')
+                    
+                }).catch(()=>{
+                    req.flash('error_msg','Erro ao registrar novo usuário')
+                    res.redirect('/user/')
+                })
+            }
 
+        })
+    }
 
-    const newUser = new Usuario({
-        nome: req.body.nome,
-        email:req.body.email,
-        nasc: req.body.nasc,
-        senha: req.body.senha
-    })
-    newUser.save().then(()=>{
-        req.flash('success_msg','Sucesso ao registrar o novo usuário')
-        res.redirect('/user/')
-        
-    }).catch(()=>{
-        req.flash('error_msg','Erro ao registrar novo usuário')
-        res.redirect('/user/')
-    })
 })
 
 
