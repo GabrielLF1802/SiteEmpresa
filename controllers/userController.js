a602de6220c2a5dd08b24c6368202fa7846f9e58
Gabriel <gabriellourencofaria@gmail.com>
2025-08-01 11:13:33 -0300

    Criação usercontroller e modificação utilizando POO

---
+++ controllers/userController.js
@@ -0,0 +1,68 @@
+// Imports
+const mongoose= require('mongoose')
+require('../models/user')
+const Usuario= mongoose.model('usuarios')
+const bcrypt= require('bcryptjs')
+
+class UserController{
+    async register (req,res){
+        const erros = this.ValidUser(req.body)
+        
+        if(erros.length>0){
+            return res.render('user/register', {erros})
+        }
+        try{
+            const UserExist= await Usuario.findOne({email:req.body.email})
+            if(UserExist){
+                req.flash('error_msg','Email já cadastrado')
+                return res.render('user/register')
+            }
+            const hashpassword= await this.gerarhash(req.body.senha)
+            const newUser= new Usuario({
+                nome:req.body.nome,
+                senha:hashpassword,
+                email:req.body.email,
+                nasc:req.body.nasc
+                })
+            await newUser.save()
+            console.log('Sucesso')
+            req.flash('success_msg','Novo usuário registrado com sucesso')
+            res.render('home')
+        }catch(err){
+            console.log(err)
+            req.flash('error_msg','Erro ao registrar novo usuário')
+            res.render('home')
+
+        }
+    }
+    ValidUser(data){
+        const erros= []
+        if(!data.email|| data.email== undefined|| data.email== null){
+            erros.push({texto:'Email inválido'})
+        }
+        if(!data.nome|| data.nome== undefined|| data.nome== null){
+            erros.push({texto:'Nome inválido'})
+        }
+        if(!data.senha||data.senha==undefined||data.senha==null){
+            erros.push({texto:'Senha inválida'})
+        }else{
+            if(data.senha.length<4){
+                erros.push({texto:'Senha muito curta'})
+            }
+            if(data.senha != data.senha2){
+                erros.push({texto:'As senhas não conhecidem'})
+            }
+        }
+        if(!data.nasc|| data.nasc== undefined || data.nasc==null){
+            erros.push({texto:'Data de nasc. inválida'})
+        }
+        return erros
+    }
+    async gerarhash(senha){
+        const salt= await bcrypt.genSalt(10)
+        const hash= await bcrypt.hash(senha,salt)
+        return hash
+    }
+
+}
+module.exports= new UserController()
--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,5 @@
+// Imports
+
 const express = require('express')
 const router = express.Router()
 const mongoose= require('mongoose')
@@ -6,7 +8,8 @@ const Usuario = mongoose.model('usuarios')
 const bcrypt= require('bcryptjs')
 const passport= require('passport')
 
-
+// Controllers
+const UserController= require('../controllers/userController')
 
 
 // Routes Usuário
@@ -19,66 +22,10 @@ router.get('/',(req,res)=>{
 router.get('/register',(req,res)=>{
     res.render('user/register')
 })
+
+
 router.post('/register',(req,res)=>{
-    // Possíveis Erros
-    erros= []
-    if(typeof req.body.senha==undefined|| !req.body.senha|| req.body.senha == null){
-        erros.push({texto:'Senha Inválida'})
-    }
-    if (!req.body.nome|| typeof req.body.nome == undefined || req.body.nome== null){
-        erros.push({texto:'Nome Inválido'})
-    }
-    if(!req.body.email||typeof req.body.email== undefined|| req.body.email== null){
-        erros.push({texto:'Email Inválido'})
-    }
-    if(!req.body.nasc|| typeof req.body.nasc== undefined|| req.body.nasc== null){
-        erros.push({texto:'Data de nascimento Inválida'})
-    }
-    if(req.body.senha != req.body.senha2){
-        erros.push({texto:'As senhas não conhecidem'})
-    }
-    if(req.body.senha.length< 4){
-        erros.push({texto:'Senha muito curta'})
-    }
-    if(erros.length>0){
-        res.render('user/register',{erros:erros})
-    }else{
-        Usuario.findOne({email:req.body.email}).then((usuario)=>{
-            if(usuario){
-                req.flash('error_msg','Email já cadastrado')
-                res.redirect('/user/register')
-            }else{
-                const newUser = new Usuario({
-                    nome: req.body.nome,
-                    email:req.body.email,
-                    nasc: req.body.nasc,
-                    senha: req.body.senha
-                })
-                bcrypt.genSalt(10,(erro, salt)=>{
-                    bcrypt.hash(newUser.senha, salt, (erro, hash)=>{
-                        if(erro){
-                            req.flash('error_msg','Erro ao registrar')
-                            res.redirect('/user/register')
-                        }
-                        newUser.senha= hash
-                        newUser.save().then(()=>{
-                            req.flash('success_msg','Sucesso ao registrar o novo usuário')
-                            res.redirect('/user/')
-                            
-                        }).catch(()=>{
-                            req.flash('error_msg','Erro ao registrar novo usuário')
-                            res.redirect('/user/register')
-                        })
-                    })
-                })
-            }
-
-        }).catch((err)=>{
-            req.flash('error_msg','Erro ao registrar novo user')
-            res.redirect('/user/register')
-        })
-    }
-
+    UserController.register(req,res)
 })
 
 
