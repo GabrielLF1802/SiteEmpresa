d00e736a0f6aa80bf46985a640972ed7af135ef9
Gabriel <gabriellourencofaria@gmail.com>
2025-08-12 20:27:20 -0300

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './views/**/*.handlebars',                  
+    './views/**/*.handlebars',
+    "./views/**/*.hbs"                  
   ],
   theme: {
     extend: {},
